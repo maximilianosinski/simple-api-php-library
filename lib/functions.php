5f91bd5cc91cb9adb402858fe56301f5f92d72bd
Maximilian Osinski <99099949+maximilianosinski@users.noreply.github.com>
2022-05-09 17:26:45 +0200

    Added more functions.

--- lib/functions.php
+++ lib/functions.php
@@ -11,6 +11,23 @@ class Responses{
             "invalidParameter" => $InvalidParameter
         );
     }
+    public static function ReturnByResponse($Response){
+        $Success = (bool)$Response["response"]["code"];
+        $Status = $Response["response"]["status"];
+        $Code = $Response["response"]["code"];
+        $Message = $Response["response"]["message"];
+        $Data = $Response["response"]["data"];
+        $InvalidParameter = $Response["response"]["invalidParameter"];
+        return array
+        (
+            "success" => $Success,
+            "status" => $Status,
+            "code" => $Code,
+            "message" => $Message,
+            "data" => $Data,
+            "invalidParameter" => $InvalidParameter
+        );
+    }
     public static function ResponseByReturn($Return){
         $Status = $Return["status"];
         $Code = $Return["code"];
@@ -45,6 +62,63 @@ class Responses{
         ), JSON_PRETTY_PRINT);
     }
 }
+class Requests{
+    public static function Get($URL, $Data){
+        $context = stream_context_create(array(
+            "http" => array("ignore_errors" => true),
+        ));
+
+        $Result = file_get_contents("$URL?$Data", false, $context);
+        return Responses::Return(true, 200, 1, "Got response.", $Result, null);
+    }
+    public static function GetArray($URL, $Data){
+        $context = stream_context_create(array(
+            "http" => array("ignore_errors" => true),
+        ));
+
+        $Result = json_decode(file_get_contents("$URL?$Data", false, $context), true);
+        if($Result != null){
+            return Responses::Return(true, 200, 1, "Got response.", $Result, null);
+        } else {return Responses::Return(true, 200, 1, "Response gave back invalid JSON.", "failed:response:json:invalid", "response-json");}
+    }
+    public static function Post($URL, $Data){
+        $Options = array(
+            "http" => array(
+                "header" => "Content-type: application/json\r\n",
+                "method" => "POST",
+                "content" => http_build_query($Data)
+            )
+        );
+        $Context  = stream_context_create($Options);
+        $Result = file_get_contents($URL, false, $Context);
+        if($Result != null){
+            return Responses::Return(true, 200, 1, "Got response.", $Result, null);
+        } else {return Responses::Return(false, 500, 1, "Unknown error.", $Result, null);}
+    }
+    public static function Request($URL, $Method, $Data){
+        $Options = array(
+            "http" => array(
+                "header" => "Content-type: application/json\r\n",
+                "method" => $Method,
+                "content" => http_build_query($Data)
+            )
+        );
+        $Context  = stream_context_create($Options);
+        $Result = file_get_contents($URL, false, $Context);
+        if($Result != null){
+            return Responses::Return(true, 200, 1, "Got response.", $Result, null);
+        } else {return Responses::Return(false, 500, 1, "Unknown error.", $Result, null);}
+    }
+    public static function URLExists($URL){
+        $FileHeaders = @get_headers($URL);
+        if(!$FileHeaders || $FileHeaders[0] == "HTTP/1.1 404 Not Found") {
+            return Responses::Return(false, 404, 0, "URL doesn't exist.", "failed:url:doesnt:exist", "url");
+        }
+        else {
+            return Responses::Return(true, 200, 1, "URL exists.", "success:url:exists", null);
+        }
+    }
+}
 class FileSystem{
     public static function ContentExists($File){
         if(file_exists($File)){
@@ -73,7 +147,7 @@ class FileSystem{
         if($ContentExists["success"]){
             $Content = json_decode(file_get_contents($File), true);
             if($Content != null){
-                return Responses::Return(true, 200, 1, "Got content.", $Content, null);
+                return Responses::Return(true, 200, 1, "Got array.", $Content, null);
             } else {return Responses::Return(false, 429, 0, "File contains invalid JSON Data.", "failed:file:json:data:invalid", null);}
         } else {return $ContentExists;}
     }
@@ -83,5 +157,55 @@ class FileSystem{
             unlink($File);
             return Responses::Return(true, 200, 1, "Deleted content.", "success:content:deleted", null);
         } else {return $ContentExists;}
+    }
+}
+class Typography{
+    public static function Translate($Text, $TargetLanguageCode){
+        $Request = Requests::GetArray("https://api.maxeman.com/v1/translate", "text=".urlencode($Text)."&source_lang=auto&target_lang=$TargetLanguageCode");
+        if($Request["success"]){
+            $Response = $Request["data"];
+            if($Response["response"]["code"] == 1){
+                $TranslatedText = $Response["response"]["data"];
+                return Responses::Return(true, 200, 1, "Translated text.", $TranslatedText, null);
+            } else {return Responses::ReturnByResponse($Response);}
+        } else {return $Request;}
+    }
+    public static function AutoTranslate($Text){
+        $IPGeoLocation = User::IPGeoLocation();
+        if($IPGeoLocation[0]){
+            $CountryCode = $IPGeoLocation["countryCode"];
+            $Translate = self::Translate($Text, $CountryCode);
+            if($Translate["success"]){
+                return $Translate;
+            } else {return $Translate;}
+        } else {return Responses::Return(false, 500, 0, "Unknown error.", "failed:error:unknown", null);}
+    }
+}
+class User{
+    public static function IP(){
+        return $_SERVER["REMOTE_ADDR"];
+    }
+    public static function UserAgent(){
+        return $_SERVER["HTTP_USER_AGENT"];
+    }
+    public static function IPGeoLocation(){
+        $Request = Requests::GetArray("https://ipapi.co/".self::IP()."/json/", null);
+        if($Request["success"]){
+            $Response = $Request["data"];
+            if(array_key_exists("city", $Response)){
+                $Country = $Response["country_name"];
+                $CountryCode = strtolower($Response["country_code"]);
+                $Region = $Response["region"];
+                $City = $Response["city"];
+                return array
+                (
+                    true,
+                    "country" => $Country,
+                    "countryCode" => $CountryCode,
+                    "region" => $Region,
+                    "city" => $City
+                );
+            } else {return array(false);}
+        } else {return array(false);}
     }
 }
